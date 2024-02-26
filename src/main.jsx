02b448bda10c0419e0b87ad94586e083343266e8
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-26 22:25:18 +0100

    practice react context api

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { ThemeProviderWrapper } from "./context/theme.context";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProviderWrapper>
+        <App />
+      </ThemeProviderWrapper>
+    </Router>
+  </React.StrictMode>
+);
